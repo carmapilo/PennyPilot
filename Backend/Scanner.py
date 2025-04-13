44895a597070a7ca462d63ba14de8bbe2a35dd55
ahmed-lamidi <ahmedlamidi@usf.edu>
2025-04-12 20:49:21 -0400

    Add OCR

--- Backend/Scanner.py
+++ Backend/Scanner.py
@@ -1,18 +1,47 @@
 import cv2
-import pytesseract
+
+import easyocr
 from PIL import Image
+from google import genai
+import json
 
 
-image = cv2.imread("receipts/receipt1.jpg")
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-thresh = cv2.adaptiveThreshold(
-    gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-    cv2.THRESH_BINARY, 11, 2
-)
+def function(test_image):
+    image = test_image
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    denoised = cv2.fastNlMeansDenoising(gray, h=30)
+    thresh = cv2.adaptiveThreshold(
+        denoised, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+        cv2.THRESH_BINARY, 11, 2
+    )
+    scaled = cv2.resize(thresh, None, fx=2, fy=2, interpolation=cv2.INTER_LINEAR)
 
-#Clean up the image
+    #Clean up the image
 
-text = pytesseract.image_to_string(thresh)
-print(text)
+    cv2.imwrite("thresh_output.jpg", scaled)
+    reader = easyocr.Reader(['en'])
+    result = reader.readtext(scaled)
+    result_text = ""
+    for (bbox, text, prob) in result:
+        result_text +=  "(" + text + ": " + str(prob) + ")"
+        print(text, prob)
 
-#use Pytesseract to extract the image
+
+
+
+    client = genai.Client(api_key="AIzaSyDr6fCdacS32OE0Phkvu3YorcT0-zCfKtM")
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents="Correct the possible grocery list item names and make a json output of items, prices are included and usually come before an F " \
+        "also use local grocery prices to give a realistic amount" + result_text,
+    )
+
+    cleaned = response.text.strip().strip("```json").strip("```")
+
+    # Load it as JSON
+    items = json.loads(cleaned)
+
+
+    return items
+
---
+++ Backend/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, UploadFile, File, Form
+from PIL import Image
+import numpy as np
+import cv2
+
+from Scanner import function
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.post("/test")
+async def image_scanner(    
+    prompt: str = Form(...),
+    sketch: UploadFile = File(...),
+):
+    sketch_bytes = await sketch.read()
+    nparr = np.frombuffer(sketch_bytes, np.uint8)
+
+    sketch_img = cv2.imdecode(nparr, cv2.IMREAD_COLOR) 
+
+    # Resize image
+    sketch_img = cv2.resize(sketch_img, (512, 512))
+
+    text = function(sketch_img)
+
+    return text
+
